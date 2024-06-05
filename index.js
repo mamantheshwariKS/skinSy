6ede2585b2c3e4abae7f628e874a42a04d6bb296
mamantheshwari <mamantheshwariks@gmail.com>
2024-06-05 15:51:23 +0530

    commits

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+const PORT = process.env.PORT
+const database = require('./src/utils/database')
+const DB = process.env.DB
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+database(DB).then(() => {
+  app.listen(PORT, () => {
+    console.log("server is running in the port 5050")
+  })
+}).catch((error) => {
+  console.error("Server startup failed due to database connection error", error);
+});
---
+++ src/utils/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+// const DB = process.env.DB
+
+const database = async (DB) => {
+  try {
+    await mongoose.connect(DB, { useUnifiedTopology: true, useNewUrlParser: true });
+    console.log("DB Connected")
+  } catch (error) {
+    (console.error("failed to connect", error))
+  }
+}
+module.exports = database;
